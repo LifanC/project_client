8baec7e53faf0a6a1d9460be89f74e5b617c591d
luke chen <bi1011105@gmail.com>
2024-01-20 16:43:00 +0800

    2024-01-20 Luke 增加時間

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <script setup>
 
+const path_ = ref('/')
+const time = ref('')
+
+setInterval(() => {
+  let NowDate = new Date()
+  let Y = NowDate.getFullYear()
+  let M = NowDate.getMonth() + 1
+  let D = NowDate.getDate()
+  let h = NowDate.getHours()
+  let m = NowDate.getMinutes()
+  let s = NowDate.getSeconds()
+  time.value = zeroPadding(Y, M, D, h, m, s)
+}, 1000)
+
+const zeroPadding = (Y, M, D, h, m, s) => {
+  let MzeroPadding = String(M).padStart(2, '0')
+  let DzeroPadding = String(D).padStart(2, '0')
+  let hzeroPadding = String(h).padStart(2, '0')
+  let mzeroPadding = String(m).padStart(2, '0')
+  let szeroPadding = String(s).padStart(2, '0')
+  let YMD = Y + '/' + MzeroPadding + '/' + DzeroPadding
+  let hms = hzeroPadding + ':' + mzeroPadding + ':' + szeroPadding
+  return YMD + ' ' + hms
+}
+
 </script>
 
 <template>
-
+  <el-breadcrumb separator="/">
+    <el-breadcrumb-item :to="{ path: path_ }">Home</el-breadcrumb-item>
+    <el-breadcrumb-item>...</el-breadcrumb-item>
+  </el-breadcrumb>
+  <el-text>{{ time }}</el-text>
+  <el-divider/>
+  <router-view/>
 </template>
 
 <style scoped>
