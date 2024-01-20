474e0ae340e0b90a42285501580afd49f621716a
luke chen <bi1011105@gmail.com>
2024-01-20 14:08:18 +0800

    2024-01-20 Luke重新

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PubSub from 'pubsub-js'
 import Router from './router'
+import axios from "axios";
 
 import ElementPlus from 'element-plus'
 
 const app = createApp(App)
 app.use(Router)
-app.use(ElementPlus, PubSub, { size: 'small', zIndex: 3000 })
+app.use(ElementPlus, PubSub, axios, { size: 'small', zIndex: 3000 })
 app.mount('#app')
